3cccb84a688259a89dbb6b9487bd44e42d61b1f1
li-evelyn <lievelynpl@gmail.com>
2021-09-19 04:09:24 -0400

    it's 4am why

--- src/App.js
+++ src/App.js
@@ -8,16 +8,17 @@ import music from "./YMCA.wav";
 
 function App() {
   const [currentStep, setCurrentStep] = useState(-1);
-
-  const URL = "https://teachablemachine.withgoogle.com/models/iVB1AnIP3/";
-  let model, ctx, webcam, labelContainer, maxPredictions;
-  let stepScores = {
+  const [stepScores, setStepScores] = useState({
     "Y" : 0,
     "M" : 0,
     "C" : 0,
     "A" : 0,
     "clap" : 0,
-  };
+  })
+  // const [score, setScore] = useState(0);
+
+  const URL = "https://teachablemachine.withgoogle.com/models/iVB1AnIP3/";
+  let model, ctx, webcam, labelContainer, maxPredictions;
   // let audio = new Audio("./YMCA.mp3");
   const SONG = dance.ymca;
 
@@ -35,7 +36,6 @@ function App() {
     // Convenience function to setup a webcam
     const size = 400;
     const flip = true; // whether to flip the webcam
-    setCurrentStep(0);
     webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
     await webcam.setup(); // request access to the webcam
     await webcam.play();
@@ -51,10 +51,17 @@ function App() {
     }
     // audio.play();
     document.getElementById("audio1").play();
+    setCurrentStep(0);
   }
 
   async function loop() {
-    webcam.update(); // update the webcam frame
+    // if (webcam) {
+      webcam.update()
+    // } else {
+    //   webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
+    //   await webcam.setup()
+    //   await webcam.play();
+    // } // update the webcam frame
     await predict();
     window.requestAnimationFrame(loop);
   }
@@ -71,8 +78,18 @@ function App() {
         let prob = prediction[i].probability.toFixed(2);
         const classPrediction = step + ": " + prob;
         labelContainer.childNodes[i].innerHTML = classPrediction;
-        stepScores[step] = prob > stepScores[step] ? prob : stepScores[step];
-        console.log(stepScores);
+        // if (currentStep >= 0) {
+        //   console.log("got in one layer");
+        //   console.log("step: " + step + " current: " + dance.ymca.timings[currentStep].pose);
+        //   if (step == dance.ymca.timings[currentStep].pose) {
+        //     setScore(prob > score ? prob : score);
+        //     console.log("hello" + prob > score ? prob : score);
+        //   }
+        // }
+        let burnerObject = stepScores;
+        burnerObject[step] = prob > stepScores[step] ? prob : stepScores[step];
+        setStepScores(burnerObject);
+        console.log("in predict ", stepScores);
     }
 
     // finally draw the poses
@@ -95,29 +112,52 @@ function App() {
     let pose = dance.ymca.timings[currentStep].pose;
     let elem = document.getElementById("score");
     // do a different case for clapping maybe? for sound - rn it's just a generic clapping pose which i guess we can fall back on if we need to do so
-    console.log(stepScores);
-    if (stepScores[pose] >= 0.5) {
+    console.log("calculation: ", stepScores);
+    let score = stepScores[pose];
+    if (score >= 0.5) {
       elem.innerHTML = "Perfect";
-    } else if (stepScores[pose] >= 0.25) {
+    } else if (score >= 0.25) {
       elem.innerHTML = "Good";
-    } else if (stepScores[pose] >= 0.1) {
+    } else if (score >= 0.1) {
       elem.innerHTML = "OK";
     } else {
       elem.innerHTML = "Miss";
     }
-    for (let score in stepScores) { // reset for next step
-      stepScores[score] = 0;
-    }
+    setStepScores({
+      "Y" : 0,
+      "M" : 0,
+      "C" : 0,
+      "A" : 0,
+      "clap" : 0,
+    });
   }
   
   // useEffect(() => {
   //   audio.load();
   // });
 
+  // useEffect(() => {
+  //   if (currentStep >= 0) {
+  //     document.getElementById("currentStep").innerHTML = dance.ymca.timings[currentStep].pose;
+  //     const interval = setInterval(() => {
+  //       loop();
+  //     }, 5);
+  //     const timer = setTimeout(() => {
+  //       if (currentStep < dance.ymca.timings.length - 1) {
+  //         // do score calculation here
+  //         calculateScore();
+  //         window.clearInterval(interval);
+  //         setCurrentStep(currentStep + 1);
+  //       }
+  //     }, dance.ymca.timings[currentStep].beats * 60000/(dance.ymca.bpm));
+  //     return () => {
+  //       clearTimeout(timer);
+  //     }
+  //   }
+  // }, [currentStep]);
+
   useEffect(() => {
-    console.log("hello");
     if (currentStep >= 0) {
-      console.log(currentStep + ": " + dance.ymca.timings[currentStep].pose + ": " + dance.ymca.timings[currentStep].beats * 60000/(dance.ymca.bpm));
       document.getElementById("currentStep").innerHTML = dance.ymca.timings[currentStep].pose;
       const timer = setTimeout(() => {
         if (currentStep < dance.ymca.timings.length - 1) {
