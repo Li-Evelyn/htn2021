a77ea3b5927f273f232476e08d14694058598164
li-evelyn <lievelynpl@gmail.com>
2021-09-19 02:19:45 -0400

    still in debug mode largely, got timings and tsfl working on getting the scoring to update correctly

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ import * as dance from './dance.json';
 import music from "./YMCA.wav";
 
 function App() {
+  const [currentStep, setCurrentStep] = useState(-1);
 
-  const URL = "https://teachablemachine.withgoogle.com/models/nMEk7cNI4/";
-  let model, ctx, webcam, labelContainer, maxPredictions, music;
+  const URL = "https://teachablemachine.withgoogle.com/models/iVB1AnIP3/";
+  let model, ctx, webcam, labelContainer, maxPredictions;
+  let stepScores = {
+    "Y" : 0,
+    "M" : 0,
+    "C" : 0,
+    "A" : 0,
+    "clap" : 0,
+  };
   // let audio = new Audio("./YMCA.mp3");
+  const SONG = dance.ymca;
 
 
   async function init() {
@@ -26,10 +35,9 @@ function App() {
     // Convenience function to setup a webcam
     const size = 400;
     const flip = true; // whether to flip the webcam
+    setCurrentStep(0);
     webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
-    console.log(webcam);
     await webcam.setup(); // request access to the webcam
-    console.log(webcam);
     await webcam.play();
     window.requestAnimationFrame(loop);
 
@@ -42,11 +50,10 @@ function App() {
         labelContainer.appendChild(document.createElement("div")); // predictions
     }
     // audio.play();
-    document.getElementById('audio1').play();
-    console.log(dance);
+    document.getElementById("audio1").play();
   }
 
-  async function loop(timestamp) {
+  async function loop() {
     webcam.update(); // update the webcam frame
     await predict();
     window.requestAnimationFrame(loop);
@@ -60,9 +67,12 @@ function App() {
     const prediction = await model.predict(posenetOutput);
 
     for (let i = 0; i < maxPredictions; i++) {
-        const classPrediction =
-            prediction[i].className + ": " + prediction[i].probability.toFixed(2); // actual prediction value
+        let step = prediction[i].className;
+        let prob = prediction[i].probability.toFixed(2);
+        const classPrediction = step + ": " + prob;
         labelContainer.childNodes[i].innerHTML = classPrediction;
+        stepScores[step] = prob > stepScores[step] ? prob : stepScores[step];
+        console.log(stepScores);
     }
 
     // finally draw the poses
@@ -80,10 +90,47 @@ function App() {
         }
     }
   }
+
+  function calculateScore() {
+    let pose = dance.ymca.timings[currentStep].pose;
+    let elem = document.getElementById("score");
+    // do a different case for clapping maybe? for sound - rn it's just a generic clapping pose which i guess we can fall back on if we need to do so
+    console.log(stepScores);
+    if (stepScores[pose] >= 0.5) {
+      elem.innerHTML = "Perfect";
+    } else if (stepScores[pose] >= 0.25) {
+      elem.innerHTML = "Good";
+    } else if (stepScores[pose] >= 0.1) {
+      elem.innerHTML = "OK";
+    } else {
+      elem.innerHTML = "Miss";
+    }
+    for (let score in stepScores) { // reset for next step
+      stepScores[score] = 0;
+    }
+  }
   
   // useEffect(() => {
   //   audio.load();
   // });
+
+  useEffect(() => {
+    console.log("hello");
+    if (currentStep >= 0) {
+      console.log(currentStep + ": " + dance.ymca.timings[currentStep].pose + ": " + dance.ymca.timings[currentStep].beats * 60000/(dance.ymca.bpm));
+      document.getElementById("currentStep").innerHTML = dance.ymca.timings[currentStep].pose;
+      const timer = setTimeout(() => {
+        if (currentStep < dance.ymca.timings.length - 1) {
+          // do score calculation here
+          calculateScore();
+          setCurrentStep(currentStep + 1);
+        }
+      }, dance.ymca.timings[currentStep].beats * 60000/(dance.ymca.bpm));
+      return () => {
+        clearTimeout(timer);
+      }
+    }
+  }, [currentStep]);
 
   return (
     <div className="App">
@@ -92,6 +139,8 @@ function App() {
       <audio id="audio1" controls="controls" preload="auto" src="http://freewavesamples.com/files/Korg-Triton-Slow-Choir-ST-C4.wav" type="audio/wav"></audio>
       <button type="button" onClick={init}>Start</button>
       <div id="label-container"></div>
+      <div id="currentStep"></div>
+      <div id="score"></div>
     </div>
   );
 }
